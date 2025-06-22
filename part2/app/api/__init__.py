c2dd66972e2cfe119b58ac9ce2915930d7e9274c
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-23 01:30:49 +0200

    i add 4 import and two lines

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -1,2 +1,7 @@
-from .amenities import api as amenities_ns
-api.add_namespace(review_ns, path='/api/v1/amenities')
+from flask_restx import Namespace, Api
+from app import api 
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns 
+
+amenities_ns = Namespace('amenities')
+api.add_namespace(amenities_ns, path='/api/v1/amenities')
