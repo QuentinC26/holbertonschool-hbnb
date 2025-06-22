a6341fd62ed821aeb014dcdeadbd715e16845e62
QuentinC26 <q.zuzlewski@gmail.com>
2025-06-22 11:33:01 +0200

    i add a /users part

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -38,3 +38,9 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
+@api.route('/users', methods=['GET'])
+class UserResource(Resource):
+    @api.response(200, 'OK')
+    def users():
+        return list(users.keys())
