da93a7900e471625954e64e8a4ae35e2693905fb
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-01 15:19:15 +0200

    i add the from import bcrypt and the line bcrypt = Bcrypt()

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from flask_bcrypt import Bcrypt
 
 def create_app():
     app = Flask(__name__)
@@ -8,4 +9,5 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    bcrypt = Bcrypt()
     return app
