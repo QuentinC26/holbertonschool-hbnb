3994d75848ba835da786f08a2b30bd3b4e91e6fb
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-02 09:26:53 +0200

    i add the line bcrypt.init_app(app)

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,11 +3,13 @@ from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
+
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
-    bcrypt = Bcrypt()
+    bcrypt.init_app(app)
     return app
