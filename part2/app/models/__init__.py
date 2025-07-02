e578fd1290e6ffdd2d84152721696eeb7c5d7a4e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-02 15:15:14 +0200

    i remove the storage part

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -4,9 +4,5 @@ from .review import Review
 from .amenity import Amenity
 from .base_model import BaseModel
 
-# Stockage global (ex: DBStorage ou FileStorage)
-from models.storage import DBStorage
-storage = DBStorage()
-
 # Permet d'importer directement : from models import User
 __all__ = ['User', 'Place', 'Review', 'Amenity', 'BaseModel']
