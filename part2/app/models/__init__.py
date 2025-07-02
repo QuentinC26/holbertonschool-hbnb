170660017896bc25753840edfcf0f88ae4dbdc8b
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-07-02 11:08:59 +0200

    correct the init of differents taks

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -1,4 +1,12 @@
-from .user import User
-from .place import Place
-from .review import Review
-from .amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+# Stockage global (ex: DBStorage ou FileStorage)
+from models.storage import DBStorage
+storage = DBStorage()
+
+# Permet d'importer directement : from models import User
+__all__ = ['User', 'Place', 'Review', 'Amenity', 'BaseModel']
