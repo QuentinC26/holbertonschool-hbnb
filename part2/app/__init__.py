473bf5b9579d2f5cb0652d8a3a2fd43ee69693b9
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-07-03 21:15:23 +0200

    Ajout des namespaces pour les lieux, les commodités et les avis dans l'API HBnB

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as place_ns
+from app.api.v1.amenities import api as amenity_ns
 
 
 import config
@@ -15,4 +17,7 @@ def create_app(config_class=config.DevelopmentConfig):
     jwt.init_app(app)
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(place_ns, path='/api/v1/places')
+    api.add_namespace(amenity_ns, path='/api/v1/amenities')
+    api.add_namespace(review_ns, path='/api/v1/reviews')
     return app
