bda4b75e086a43ff5a64981a357fce39559f4518
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-07-03 14:27:50 +0200

    correct the import of __init__.py of app

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-from flask_jwt_extended import JWTManager
 
-from config import DevelopmentConfig
+
+import config
 
 jwt = JWTManager()
 
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     app.config.from_object(config_class)
     jwt.init_app(app)
 
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
