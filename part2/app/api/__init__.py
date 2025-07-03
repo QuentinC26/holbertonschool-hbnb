697cb16316c1d30566fa3e5b5b813f44f28b0e34
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-07-03 21:51:53 +0200

    Ajout de l'importation du namespace des avis et correction de la référence dans l'API HBnB

--- part2/app/api/__init__.py
+++ part2/app/api/__init__.py
@@ -9,13 +9,14 @@ api = Api(
 )
 
 # save namespaces
-from .v1.reviews import api as reviews_ns
 from .v1.users import api as users_ns
 from .v1.places import api as places_ns
 from .v1.amenities import api as amenities_ns
+from .v1.reviews import api as review_ns
+
 
 # root
 api.add_namespace(users_ns, path="/api/v1/users")
 api.add_namespace(places_ns, path="/api/v1/places")
-api.add_namespace(reviews_ns, path="/api/v1/reviews")
 api.add_namespace(amenities_ns, path="/api/v1/amenities")
+api.add_namespace(review_ns, path="/api/v1/reviews")
