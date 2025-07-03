286139fb12d85af8665f371b15b497a1fa9286d7
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-03 23:32:55 +0200

    i add config part

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-
-
+from flask_bcrypt import Bcrypt
 import config
 
-jwt = JWTManager()
+bcrypt = Bcrypt()
 
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     app.config.from_object(config_class)
-    jwt.init_app(app)
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    bcrypt.init_app(app)
     return app
