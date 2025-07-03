23755174fab26a83510146277ef86ad7b5dbdeaf
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-07-03 21:52:01 +0200

    Ajout de l'importation du namespace des avis dans l'API HBnB

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,6 +4,7 @@ from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as place_ns
 from app.api.v1.amenities import api as amenity_ns
+from app.api.v1.reviews import api as review_ns
 
 
 import config
