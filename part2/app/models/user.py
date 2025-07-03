73e62b9877138728cc10d6a40c0a2ced14716c89
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-03 23:32:14 +0200

    i add def hash_password and verify_password

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -15,3 +15,12 @@ class User(BaseModel):
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        self.password = password
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
