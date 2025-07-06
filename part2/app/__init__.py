86689b333b28f8956bf97c421983baf156c98dd6
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 14:35:56 +0200

    i correct my merge mistake

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,20 @@
-from app import create_app
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_restx import Api
+from app.api.v1.users import api as users_ns
+from flask_bcrypt import Bcrypt
+import config
 
-app = create_app()
+jwt = JWTManager()
+bcrypt = Bcrypt()
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+def create_app(config_class=config.DevelopmentConfig):
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    app.config.from_object(config_class)
+    jwt.init_app(app)
+
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(users_ns, path='/api/v1/users')
+    bcrypt.init_app(app)
+    return app
