d3089e05cbcb0a6f6e7ccae594e6401fd759fb55
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 16:05:47 +0200

    i add modification given by Holberton

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,10 +3,12 @@ from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 import config
 
 jwt = JWTManager()
 bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
@@ -17,4 +19,5 @@ def create_app(config_class=config.DevelopmentConfig):
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(users_ns, path='/api/v1/users')
     bcrypt.init_app(app)
+    db.init_app(app)
     return app
