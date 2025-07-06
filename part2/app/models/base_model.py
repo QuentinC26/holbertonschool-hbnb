9fc727baab865f2e590f2838de7ab9b5fd47623e
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-06 16:08:21 +0200

    i replace inmemory class by sqlalchemy class

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 
-class BaseModel:
+class BaseModel(db.Model):
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
