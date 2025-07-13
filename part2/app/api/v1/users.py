5d203870476fc3af4d80b513f2cc1840954e3759
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-13 18:17:23 +0200

    i add one import

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.services import facade
 from flask_bcrypt import Bcrypt
 
