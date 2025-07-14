3aaa05ab9007d1cfeeae638e3340e7a817c65b2f
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-14 11:20:22 +0200

    i copy the part2 work and i paste in part3

---
+++ part3/app/models/user.py
@@ -0,0 +1,36 @@
+import re
+from app import db, bcrypt
+import uuid
+from datetime import datetime
+from app.models.base_model import BaseModel
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
+        super().__init__()
+        if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValueError("Email invalide")
+        if len(first_name) > 50 or len(last_name) > 50:
+            raise ValueError("Nom trop long (max 50 caractères)")
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.password = password
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
