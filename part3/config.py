4a4b8bdc5ec979db674a04917e247456cacfaa8d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-14 11:15:03 +0200

    i copy the part2 work in part3

---
+++ part3/config.py
@@ -0,0 +1,14 @@
+import os
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Fals
+    JWT_SECRET_KEY = 'your-secret-key'
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
