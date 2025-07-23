3cd8e518487b48adfaa15cf93f57affdabca3abc
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-23 14:39:50 +0200

    i move jwt.init.app(app) after the api.add_namespace

--- part4/app/__init__.py
+++ part4/app/__init__.py
@@ -16,12 +16,12 @@ def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-    jwt.init_app(app)
 
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(place_ns, path='/api/v1/places')
     api.add_namespace(amenity_ns, path='/api/v1/amenities')
     api.add_namespace(review_ns, path='/api/v1/reviews')
+    jwt.init_app(app)
     bcrypt.init_app(app)
     return app
