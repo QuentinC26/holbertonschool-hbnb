b231f86830d5caf9627c6ba76ff8de2dfd062e4d
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-24 13:45:15 +0200

    i add tablename with the colum of table

--- part4/app/models/user.py
+++ part4/app/models/user.py
@@ -4,17 +4,13 @@ from app.models.base_model import BaseModel
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, password, is_admin=False):
-        super().__init__()
-        if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            raise ValueError("Email invalide")
-        if len(first_name) > 50 or len(last_name) > 50:
-            raise ValueError("Nom trop long (max 50 caractères)")
+        __tablename__ = 'users'
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
-        self.password = password
+        first_name = db.Column(db.String(50), nullable=False)
+        last_name = db.Column(db.String(50), nullable=False)
+        email = db.Column(db.String(120), nullable=False, unique=True)
+        password = db.Column(db.String(128), nullable=False)
+        is_admin = db.Column(db.Boolean, default=False)
 
     def hash_password(self, password):
         from app import bcrypt
