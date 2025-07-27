bc4b2ad212b3157626a96f2ca62c2be5a4e6961b
QuentinC26 <q.zuzlewski@gmail.com>
2025-07-27 14:48:53 +0200

    i add amenity class

--- part4/app/models/amenity.py
+++ part4/app/models/amenity.py
@@ -1,6 +1,11 @@
 from app.models.baseclass import BaseModel
 
 class Amenity(BaseModel):
+    from app import db
+    __tablename__ = 'amenities'
+
+    name = db.Column(db.String(50), nullable=False)
+
     def __init__(self, name):
         super().__init__()
         if not name or len(name) > 50:
