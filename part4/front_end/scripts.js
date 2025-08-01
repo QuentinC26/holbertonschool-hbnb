9d23c4679d992e847a05878272dc0f4f46aaecf0
QuentinC26 <q.zuzlewski@gmail.com>
2025-08-01 08:26:39 +0200

    i add 4 functions

--- part4/front_end/scripts.js
+++ part4/front_end/scripts.js
@@ -38,6 +38,7 @@ async function loginUser(email, password) {
 function checkAuthentication() {
     const token = getCookie('token');
     const loginLink = document.getElementById('login-link');
+    const addReviewSection = document.getElementById('add-review');
 
     if (!token) {
         loginLink.style.display = 'block';
@@ -45,6 +46,14 @@ function checkAuthentication() {
         loginLink.style.display = 'none';
         // Fetch places data if the user is authenticated
         fetchPlaces(token);
+    }
+
+    if (!token) {
+        addReviewSection.style.display = 'none';
+    } else {
+        addReviewSection.style.display = 'block';
+        // Store the token for later use
+        fetchPlaceDetails(token, placeId);
     }
 }
 
@@ -138,3 +147,85 @@ document.getElementById('price-filter').addEventListener('change', (event) => {
         }
     });
 });
+
+function getPlaceIdFromURL() {
+    // Extract the place ID from window.location.search
+    PlaceId = window.location.search(place_id)
+}
+
+async function fetchPlaceDetails(token, PlaceId) {
+    // Make a GET request to fetch place details
+    // Include the token in the Authorization header
+    // Handle the response and pass the data to displayPlaceDetails function
+    const response = await fetch('http://127.0.0.1:5000/api/v1/get/' + PlaceId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": "Bearer" + " " + token
+        },
+    })
+    if (response.ok) {
+      const data = await response.json();
+      displayPlaceDetails(data)
+    } else {
+        alert('Access to this place failed: ' + response.statusText);
+    }
+  }
+
+
+function displayPlaceDetails(place) {
+    // Clear the current content of the place details section
+    // Create elements to display the place details (name, description, price, amenities and reviews)
+    // Append the created elements to the place details section
+    document.getElementById(placeId).innerHTML = '';
+    const placesbyid = document.getElementById(placeId);
+    for (index3 in place) {
+      const places = place[index3];
+      let div = document.createElement("div");
+      let title = document.createElement("h2");
+      let space_one = document.createElement("br");
+      let description = document.createElement("p");
+      let space_two = document.createElement("br");
+      let price = document.createElement("p");
+      let space_three = document.createElement("br");
+      let amenities = document.createElement("p");
+      let space_four = document.createElement("br");
+      let reviews = document.createElement("p")
+      let space_five = document.createElement("br");
+      let add_reviews_link = document.createElement("a");
+      let details_img = document.createElement("img");
+
+      title.innerText = places[index3].title;
+      description.innerText = places[index3].description;
+      price.innerText = `$${places[index3].price}`;
+      amenities.innerText = places[index3].amenities;
+      reviews.innerText = places[index3].reviews;
+
+      add_reviews_link.href = "add_reviews.html";
+      add_reviews_link.classList.add("details-button");
+
+      details_img.src = "view_details.png";
+      details_img.alt = "View details";
+      view_details_link.appendChild(details_img);
+
+      div.append(title);
+      div.append(space_one);
+      div.append(description);
+      div.append(space_two);
+      div.append(price);
+      div.append(space_three)
+      div.append(amenities)
+      div.append(space_four)
+      div.append(reviews)
+      div.append(space_five)
+      div.append(view_details_link);
+      
+      placesbyid.append(div)
+      div.setAttribute("data-title", places[index3].title);
+      div.setAttribute("data-description", places[index3].description);
+      div.setAttribute("data-price", places[index3].price);
+      div.setAttribute("data-amenities", places[index3].amenities);
+      div.setAttribute("data-reviews", places[index3].reviews);
+      placesbyid.appendChild(div);
+    }
+}
